dea5df9b945366902da1b75524d35d09842677b3
josue jouvin <josuejouvin27@gmail.com>
2023-12-04 19:15:46 -0500

    Solucion Reto 2

---
+++ Retos/Reto-02/index.js
@@ -0,0 +1,17 @@
+const gifts = ["tren", "oso", "pelota"];
+const materials = "tronesa";
+
+function manufacture(gifts, materials) {
+  const giftBox = [];
+  for (const gift of gifts) {
+   
+    if (gift.split("").every((letter) => materials.includes(letter))) {
+      console.log(gift)
+      giftBox.push(gift);
+    }
+  }
+
+  return giftBox;
+}
+
+console.log(manufacture(gifts, materials));
